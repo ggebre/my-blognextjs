cf3476117f86cbaa99eb70376c044fee69c2ba0a
Getu Gebre <getu.gebre.gg@gmail.com>
2023-08-09 08:03:38 -0500

    configure tailwind css to project

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@
 export default function Home() {
   return (
     <main>
-        <h1>My Blog</h1>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </main>
   )
 }
