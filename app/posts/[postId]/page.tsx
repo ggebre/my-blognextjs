6725e9555e2629436680500437a1990d641cf734
Getu Gebre <getu.gebre.gg@gmail.com>
2023-08-14 11:34:43 -0500

    turn page to ssg from ssr

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -3,6 +3,14 @@ import { getSortedPostsData, getPostData } from "@/lib/posts";
 import {notFound} from 'next/navigation'
 import Link from 'next/link';
 
+export function generateStaticParams() {
+    const posts = getSortedPostsData() //deduped 
+
+    return posts.map((post) => ({
+        postId: post.id
+    }))
+}
+
 export function generateMetadata({ params }: { params : {postId: string}}) {
     const posts = getSortedPostsData() //deduped
     const { postId } = params
